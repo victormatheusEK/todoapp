1c139f020a7f17e15d5c7bb275ac9f4f33e6b0fa
Victor Matheus <victor.matheus@protonmail.com>
2022-06-27 19:43:13 -0300

    Creation of the TodoRetrieveUpdateDestroy view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,25 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .serializers import TodoSerializer
 from todo.models import Todo
 
 
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
